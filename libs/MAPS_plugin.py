05f5e7d09b1da30eb075fa662c8d3d57fddbf87c
SCWhite <sean3381212@gmail.com>
2020-12-23 08:13:02 +0000

    change Leq calculation / use minite Leq

--- libs/MAPS_plugin.py
+++ libs/MAPS_plugin.py
@@ -14,7 +14,6 @@ BAUD_RATES = 115200
 
 #pairs = datetime.now().strftime("%Y-%m-%d %H-%M").split(" ")
 
-time_slot_string = ""
 slot_count = 0
 slot_energy = 0
 Leq = 0
@@ -22,13 +21,11 @@ Leq_Max = 0
 Leq_Min = 0
 Leq_Median = 0
 
-eq_slot_count = 0
-eq_slot_energy = 0
 min_leq = 0
 
-#all_slot_energy = 0
-#all_slot_count  = 0
-#all_leq         = 0
+all_slot_energy = 0
+all_slot_count  = 0
+
 
 #notice: we use a sliding windows to calculate Max/Min/Mid
 # one goes in, one come out
@@ -41,11 +38,11 @@ dba_windows = deque(maxlen=60)
  - first we have a lot of data in 1 second
     ex: dB(A)_t0 = 50 db ~ dB(A)_tn = 60 db
 
- - second, do  "pow(10,(x/5))" to get energy of t0
+ - second, do  "pow(10,(x/10))" to get energy of t0
 
  - next, "sum up pow_t0 ~ pow_tn" and cacluate "pow_AVG" (depends on how long we want)
 
- - do a "sqrt(pow_AVG)" to get energy of this time period
+ - #(no need) do a "sqrt(pow_AVG)" to get energy of this time period
 
  - last, caculate  "log10(energy_of__t0~tn) * 10" to get Leq of this time period (dB(A))
 
@@ -55,15 +52,15 @@ dba_windows = deque(maxlen=60)
 
 
 def transfer_to_eng(x):
-    Lf = math.pow(10,(x/5))
+    #Lf = math.pow(10,(x/5))
+    Lf = math.pow(10,(x/10))
     return Lf
 
 def get_dba_data():
 
-    global slot_count, slot_energy, Leq, time_slot_string
+    global slot_count, slot_energy, Leq
     global Leq_Max, Leq_Min, Leq_Median, dba_windows
-    global eq_slot_count, eq_slot_energy, min_leq
-    #global all_slot_energy, all_slot_count, all_leq
+    global all_slot_energy, all_slot_count, min_leq
 
     while True:
         #ser = serial.Serial(MIC_COM_PORT, BAUD_RATES)
@@ -71,13 +68,9 @@ def get_dba_data():
             ser = serial.Serial(MIC_COM_PORT, BAUD_RATES)
 
             last_time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S").split(" ")[1]
-            #add a time stamp for minute_eq
-            last_time_stamp_for_minute_eq = datetime.now().strftime("%Y-%m-%d %H:%M").split(" ")[1]
 
             while True:
                 time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S").split(" ")[1]
-                #add a time stamp for minute_eq
-                time_stamp_for_minute_eq = datetime.now().strftime("%Y-%m-%d %H:%M").split(" ")[1]
 
                 if(time_stamp == last_time_stamp):
                     while ser.in_waiting:
@@ -91,50 +84,30 @@ def get_dba_data():
                         slot_energy =  slot_energy + transfer_to_eng(float(data_set[1]))
 
                         #print(time_stamp +": "+data_set[1])
-
-                        #time_slot_string = time_slot_string + str(data_set[1]) + ","
                         slot_count = slot_count + 1
-                        #all_slot_count = all_slot_count + 1
 
                 else:
                     #transfer back to dba / Leq in 1 seconds
-                    Leq = math.log10(math.sqrt(slot_energy / slot_count)) * 10
-
-                    #check eng
-                    #all_slot_energy = all_slot_energy + slot_energy
+                    #Leq = math.log10(math.sqrt(slot_energy / slot_count)) * 10
+                    Leq = math.log10(slot_energy / slot_count) * 10
 
                     #limited to 2 places
                     Leq = round(Leq,2)
                     dba_windows.append(Leq)
 
                     #this part is for calculate minute Leq
-                    if(time_stamp_for_minute_eq == last_time_stamp_for_minute_eq):
-                        eq_slot_energy = eq_slot_energy + transfer_to_eng(Leq)
-                        eq_slot_count  = eq_slot_count + 1
-                        #print("min_count:" + str(eq_slot_count) + "\n")
-                    else:
-                        min_leq = math.log10(math.sqrt(eq_slot_energy / eq_slot_count)) * 10
-                        min_leq = round(min_leq,2)
+                    #adds up every Leq in the moving windows
+                    for i in range(len(dba_windows)):
+                        #print("choose slot:" + str(dba_windows[i]))
+                        all_slot_energy = all_slot_energy + transfer_to_eng(dba_windows[i])
+                        all_slot_count = len(dba_windows)
+                    #print("all energy:" + str(all_slot_energy))
+                    #print("all count :" + str(all_slot_count))
+                    #print("this is list:")
+                    #print(dba_windows)
 
-                        #to verify min_leq / compare to every entry in one minite
-                        #all_leq = math.log10(math.sqrt(all_slot_energy / all_slot_count)) * 10
-                        #all_leq = round(all_leq,2)
-
-                        #print("eq_slot_energy   :" + str(eq_slot_energy) + "\n")
-                        #print("check_slot_energy:" + str(all_slot_energy) + "\n")
-
-                        #print("eq_slot_count   :" + str(eq_slot_count) + "\n")
-                        #print("check_slot_count:" + str(all_slot_count) + "\n")
-
-                        #print("eq_DB   :" + str(min_leq) + "\n")
-                        #print("all_DB  :" + str(all_leq) + "\n")
-
-                        eq_slot_energy = 0
-                        eq_slot_count  = 0
-                        #all_slot_energy = 0
-                        #all_slot_count  = 0
-
-                        last_time_stamp_for_minute_eq = time_stamp_for_minute_eq
+                    min_leq = math.log10(all_slot_energy / all_slot_count) * 10
+                    min_leq = round(min_leq,2)
 
                     Leq_Max = max(dba_windows)
                     Leq_Min = min(dba_windows)
@@ -149,14 +122,12 @@ def get_dba_data():
                     #print("min_leq: " + str(min_leq) + "\n")
                     #print("------------------")
 
-                    time_slot_string = ""
                     slot_energy = 0
                     slot_count  = 0
+                    all_slot_energy = 0
+                    all_slot_count  = 0
                     last_time_stamp = time_stamp
 
-                    #eq_slot_energy = 0
-                    #eq_slot_count  = 0
-                    #last_time_stamp_for_minute_eq = time_stamp_for_minute_eq
 
         except:
             ser.close()
